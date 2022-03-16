cef59d451a7ac2a266b65299d6daa7acb6e40413
Ghower <1520567597@qq.com>
2022-03-16 12:10:20 +0800

    add: [剑指 Offer 58 - I]翻转单词顺序

--- leetcode/offer/LiangGeLianBiaoDeDiYiGeGongGongJieDianLcof.java
+++ leetcode/offer/LiangGeLianBiaoDeDiYiGeGongGongJieDianLcof.java
@@ -81,17 +81,17 @@ public class LiangGeLianBiaoDeDiYiGeGongGongJieDianLcof{
  * }
  */
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if (headA == null || headB == null){
-            return null;
-        }
-        ListNode a = headA,b = headB;
-        while (a!=b){
-            a = (a == null) ? headB : a.next;
-            b = (b == null) ? headA : b.next;
-        }
-        return a;
-    }
+//    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//        if (headA == null || headB == null){
+//            return null;
+//        }
+//        ListNode a = headA,b = headB;
+//        while (a!=b){
+//            a = (a == null) ? headB : a.next;
+//            b = (b == null) ? headA : b.next;
+//        }
+//        return a;
+//    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
