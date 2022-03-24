7e579d2b77d927e1332d7af8c236d8ccacb2f49d
Ghower <1520567597@qq.com>
2022-03-24 11:31:47 +0800

    add: [661]图片平滑器

--- leetcode/TEST/TestDemo.java
+++ leetcode/TEST/TestDemo.java
@@ -14,13 +14,13 @@ public class TestDemo {
 //        System.out.println(s1.intern() == s1);
 //        String s2 = new StringBuilder("ja").append("va").toString();
 //        System.out.println(s2.intern() == s2);
-        List<String> list = Arrays.asList("dog", "over", "good");
+//        List<String> list = Arrays.asList("dog", "over", "good");
 //        list.stream().reduce((x1, x2) -> x1.length() > x2.length() ? x1:x2).ifPresent(System.out::println);
 
 //        list.stream().reduce((x1, x2) -> x1.length() == 3 ? x1:x2).ifPresent(System.out::println);
 
 //        System.out.println(list.stream().reduce(new String(),(x1, x2) -> {if (x1.equals("dog")) return x1;return x2;}));
-        System.out.println(list.stream().reduce(new String(),(x1, x2) -> x1 +x2.charAt(0),(c1,c2) -> c1+c2));
-
+//        System.out.println(list.stream().reduce(new String(),(x1, x2) -> x1 +x2.charAt(0),(c1,c2) -> c1+c2));
+        System.out.printf("%.2f",0.1+0.2);
     }
 }
